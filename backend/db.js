04942b92e36876398c20da91b946a83c6d67d81e
Saumya <7226shivam1007@gmail.com>
2024-07-03 10:36:31 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,37 @@
+
+const mongoose = requre("mongoose");
+
+mongoose.connect("")
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        Unique: true,
+        minLength: 4,
+        maxLength: 12
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 12
+
+    },
+    firstName: {
+        type: String,
+        required: true,
+        maxLength: 20
+    },
+    lastName: {
+        type: String,
+        required: true,
+        maxLength: 20
+    }
+
+})
+
+const User = mongoose.model("User", userSchema)
+module.exports = {
+    User
+}
