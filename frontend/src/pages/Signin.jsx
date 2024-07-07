7c1ab4d3de67109ba6622af96710237045968301
Saumya <7226shivam1007@gmail.com>
2024-07-08 04:04:28 +0530

    sign in

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,7 +1,61 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const Data = {
+        username: email,
+        password: password,
+      };
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/user/signin",
+        Data
+      );
+      if (response.status == 200) {
+        setEmail("");
+        setPassword("");
+      }
+    } catch (err) {
+      console.log("error while loging in");
+    }
+  };
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <h4>Enter your credentials to access your account</h4>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="email"
+          name=""
+          id="email"
+          value={email}
+        />
+        <br />
+        <label htmlFor="passwrod">Password</label>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="password"
+          id="password"
+          value={password}
+        />
+        <br />
+        <button>Sign In</button>
+        <p>not have an account?</p>
+        <Link to="/signup">Sign in</Link>
+      </form>
+    </div>
+  );
 };
 
 export default Signin;
