24662392363f66bec2730571e64850c32961179a
Saumya <7226shivam1007@gmail.com>
2024-07-13 00:09:15 +0530

    balance updated and new component

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,8 +1,26 @@
-export const Balance = ({ value }) => {
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export const Balance = () => {
+  const [balance, setBalance] = useState(0);
+
+  const fetchBalance = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/v1/account");
+      setBalance(response.data.balance);
+    } catch (error) {
+      console.error("Error fetching balance:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBalance();
+  }, []);
+
   return (
     <div className="flex">
       <div className="font-bold text-lg">Your balance</div>
-      <div className="font-semibold ml-4 text-lg">Rs {value}</div>
+      <div className="font-semibold ml-4 text-lg">Rs {balance}</div>
     </div>
   );
 };
